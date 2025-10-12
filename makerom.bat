4e60913c48be47ecc1515c384e2da770ffa5e900
91181102 <91181102a@gmail.com>
2025-10-11 22:21:16 -0300

    sem alteracoes

--- makerom.bat
+++ makerom.bat
@@ -1,7 +1,7 @@
 @echo off
 title Criando Rom MSX...
 set OPENMSX_PATH="C:\git\msx_projetos\msxbas2rom\msxbas2rom.exe"
-set BASFILE="C:\git\msx_projetos\record.bas"
+set BASFILE="C:\git\msx_projetos\bbaby.bas"
 
 echo ========================================
 echo Iniciando o MSXBas2Rom com %BASFILE%
